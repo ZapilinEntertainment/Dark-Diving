28ec50cce404090432e55401dab6442e113d3565
ZapilinEntertainment <mahariusls@yandex.ru>
2017-12-18 22:52:18 +0500

    Add files via upload

--- CameraEffects.cs
+++ CameraEffects.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class CameraEffects : MonoBehaviour {
 		public Shader m_Shader = null;
-	public float pc;
-		const float MAX_DEEP = -1000;
-		float prevHeight;
+	public float pc; // Процент погружения, 0 - на поверхности, 1 - там. куда не достаёт свет
+	const float MAX_DEEP = -2000, SUNLIGHT_LIMIT = 0.1f;
+	float prevHeight, colouringCooldown = 1, t;
 		private Material m_Material;
-	public Light seaLight;
-
+	public Light dayLight;
+	public Color normalSeaColor, mySeaColor;
+	public Material skyboxMaterial;
+	Color prevColor; 
 
 		void Start()
 		{  
@@ -18,6 +20,7 @@ public class CameraEffects : MonoBehaviour {
 				m_Material = new Material(m_Shader);
 				m_Material.name = "ImageEffectMaterial";
 				m_Material.hideFlags = HideFlags.HideAndDontSave;
+			m_Material.SetColor("_MyColor", mySeaColor);
 			}
 
 			else
@@ -25,19 +28,19 @@ public class CameraEffects : MonoBehaviour {
 				Debug.LogWarning(gameObject.name + ": Shader is not assigned. Disabling image effect.", this.gameObject);
 				enabled = false;
 			}
+		SetColors();
 		}
 
+	void Update () {
+		if (t > 0 ) {t -= Time.deltaTime;}
+	}
+
 		void OnRenderImage(RenderTexture src, RenderTexture dst)
-		{
-		
+		{		
 		if (transform.position.y != prevHeight) {
-			prevHeight = transform.position.y;
-			pc = 1 - prevHeight / MAX_DEEP;
-			if (pc < 0) pc = 0; 
-			else {if (pc > 1) pc = 1;}
-			m_Material.SetFloat("_DeepPercent", pc);
-			seaLight.intensity = pc;
+			SetColors();
 		}
+
 			if (m_Shader && m_Material)
 			{
 				Graphics.Blit(src, dst, m_Material);
@@ -57,4 +60,34 @@ public class CameraEffects : MonoBehaviour {
 				DestroyImmediate(m_Material);
 			}
 		}
+
+	void SetColors() {
+		float waterline = GameMaster.GetWaterlevel();
+		prevHeight = transform.position.y;
+		if (prevHeight >= waterline) pc = 0;
+		else {
+			if (prevHeight <= MAX_DEEP) pc = 1;
+			else pc = prevHeight / MAX_DEEP;
+		}
+		//Общее затенение
+		m_Material.SetFloat("_DeepPercent", pc);
+		// Global Illumination
+		Color environmentColor;
+		if (transform.position.y < waterline ) environmentColor = Color.Lerp(normalSeaColor, Color.black, pc);
+		else environmentColor = new Color (0.9f,0.9f,0.8f,0.1f);
+		if (environmentColor != prevColor) {
+			RenderSettings.ambientSkyColor= environmentColor;
+			skyboxMaterial.color = environmentColor;
+			prevColor = environmentColor;
+		}
+		RenderSettings.ambientIntensity=Mathf.Sin(pc * Mathf.PI);
+		RenderSettings.reflectionIntensity=(1-pc)/2;
+		//Солнечный свет
+		if (pc < SUNLIGHT_LIMIT) {
+			if (!dayLight.enabled) dayLight.enabled = true;
+			dayLight.intensity = (1 - pc/SUNLIGHT_LIMIT) *0.5f;
+		}
+		else {if (dayLight.enabled) {dayLight.intensity = 0;dayLight.enabled = false;}}
+
+	}
 }
--- TestShader.shader
+++ TestShader.shader
@@ -4,6 +4,7 @@
 	{
 		_MainTex ("Texture", 2D) = "white" {}
 		_DeepPercent ("Deep level", Range (0,1)) = 0
+		_MyColor ("Sea Color", COLOR ) = (1,1,1,1)
 	}
 	SubShader
 	{
@@ -40,13 +41,15 @@
 			
 			sampler2D _MainTex;
 			float _DeepPercent;
+			fixed4 _MyColor;
 
 			fixed4 frag (v2f i) : SV_Target
 			{
 				fixed4 col = tex2D(_MainTex, i.uv);
-				col.r *= _DeepPercent;
-				col.g *= _DeepPercent;
-				col.b *=sin (_DeepPercent * 1.57);
+				col.r *= (1 - _DeepPercent); 
+				col.g *= 0.5 + (1 - _DeepPercent)/2;
+				col.b *= 0.5 + (1 - _DeepPercent)/2;
+				if (col.a = 0) col = _MyColor;
 				return col;
 			}
 			ENDCG
